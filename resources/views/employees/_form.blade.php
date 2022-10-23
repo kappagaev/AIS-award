cb1b5ce84e8f387ebccea5b5ddfede3481439e91
kkpagaev <kapagaev@gmail.com>
2022-10-23 23:53:26 +0300

    drop boxes

--- resources/views/employees/_form.blade.php
+++ resources/views/employees/_form.blade.php
@@ -1,7 +1,13 @@
 <?php
 use App\Models\Employee;
 
-$dropFields = ['name', 'faculty', 'award', 'state_award'];
+$suggestFields = ['name'];
+$dropFields = ['faculty', 'award', 'state_award'];
+
+$faculties = ['ІАТ', 'ІАТЕ', 'ІЕЕ', 'ІМЗ', 'ІПСА', 'ІТС', 'ММІ', 'ФТІ', 'ІХФ', 'ПБФ', 'РТФ', 'ФБМІ', 'ФБТ', 'ФЕА', 'ФЕЛ', 'ФІОТ', 'ФЛ', 'ФММ', 'ФМФ', 'ФПМ', 'ФСП', 'ХТФ', 'ІСЗЗІ'];
+
+$award = ['Грамота Вченої ради (№ протоколу ВР КПІ)', 'Почесна грамота Вченої ради (№ протоколу ВР КПІ)', 'Почесна відзнака Вченої ради (№ протоколу ВР КПІ)', 'почесне звання «Заслужений викладач КПІ» (№ протоколу ВР КПІ)', 'почесне звання «Заслужений професор КПІ» (№ протоколу ВР КПІ)', 'почесне звання «Заслужений науковець КПІ» (№ протоколу ВР КПІ)', 'почесне звання «Заслужений працівник КПІ» (№ протоколу ВР КПІ)', 'почесна відзнака «Видатний діяч КПІ» (№ протоколу ВР КПІ)', 'почесне звання «Почесний доктор КПІ» (№ протоколу ВР КПІ)', 'почесна відзнака «За служіння та відданість КПІ» (№ протоколу ВР КПІ)', 'почесна відзнака «За заслуги  перед КПІ» (№ протоколу ВР КПІ)'];
+$stateAwards = ['Подяка МОН України', 'Грамота МОН України', 'Почесна грамота МОН України', 'нагрудний знак «Відмінник освіти»', 'почесне звання «Заслужений діяч науки і техніки України»', 'Подяка КМ України', 'Грамота КМ України'];
 
 ?>
 <form method="post" action="{{ $action }}">
@@ -9,7 +15,7 @@ $dropFields = ['name', 'faculty', 'award', 'state_award'];
         @method($method)
     @endif
     @csrf
-    @foreach ($dropFields as $field)
+    @foreach ($suggestFields as $field)
         <div class="form-group">
             <div class="form-group">
                 <label for="{{ $field }}">{{ Employee::attributes()[$field] }}</label>
@@ -27,21 +33,50 @@ $dropFields = ['name', 'faculty', 'award', 'state_award'];
 
         </div>
     @endForeach
+    <div class="form-group">
+        <label for="faculty">{{ Employee::attributes()['faculty'] }}</label>
+        <select name="faculty" id="faculty" class="form-control">
+            @foreach ($faculties as $faculty)
+                <option value="{{ $faculty }}" @if (isset($employee) && $employee->faculty == $faculty) selected @endif>
+                    {{ $faculty }}
+                </option>
+            @endforeach
+        </select>
+    </div>
+    <div class="form-group">
+        <label for="award">{{ Employee::attributes()['award'] }}</label>
+        <select name="award" id="award" class="form-control">
+            @foreach ($award as $award)
+                <option value="{{ $award }}" @if (isset($employee) && $employee->award == $award) selected @endif>
+                    {{ $award }}
+                </option>
+            @endforeach
+        </select>
+    </div>
+    <div class="form-group">
+        <label for="stateAward">{{ Employee::attributes()['state_award'] }}</label>
+        <select name="state_award" id="stateAward" class="form-control">
+            @foreach ($stateAwards as $stateAwards)
+                <option value="{{ $stateAwards }}" @if (isset($employee) && $employee->stateAwards == $stateAwards) selected @endif>
+                    {{ $stateAwards }}
+                </option>
+            @endforeach
+        </select>
 
-    <div class="form-group">
-        <label for="protocol">№ протоколу ВР КПІ ім. Ігоря Сікорського про відзначення</label>
-        <input type="text" class="form-control" id="protocol" name="protocol"
-            value="{{ $employee->protocol ?? '' }}">
-    </div>
-    <div class="form-group">
-        <label for="award_year">Рік відзначення КПІ</label>
-        <input type="number" min="1990" max="2022" class="form-control" id="award_year" name="award_year"
-            value="{{ $employee->award_year ?? '2022' }}">
-    </div>
-    <div class="form-group">
-        <label for="state_award_year">Рік призначення державою</label>
-        <input type="number" min="1990" max="2022" class="form-control" id="state_award_year"
-            name="state_award_year" value="{{ $employee->state_award_year ?? '2022' }}">
-    </div>
-    <button type="submit" class="btn btn-success">Відправити</button>
+        <div class="form-group">
+            <label for="protocol">№ протоколу ВР КПІ ім. Ігоря Сікорського про відзначення</label>
+            <input type="text" class="form-control" id="protocol" name="protocol"
+                value="{{ $employee->protocol ?? '' }}">
+        </div>
+        <div class="form-group">
+            <label for="award_year">Рік відзначення КПІ</label>
+            <input type="number" min="1990" max="2022" class="form-control" id="award_year" name="award_year"
+                value="{{ $employee->award_year ?? '2022' }}">
+        </div>
+        <div class="form-group">
+            <label for="state_award_year">Рік призначення державою</label>
+            <input type="number" min="1990" max="2022" class="form-control" id="state_award_year"
+                name="state_award_year" value="{{ $employee->state_award_year ?? '2022' }}">
+        </div>
+        <button type="submit" class="btn btn-success">Відправити</button>
 </form>
